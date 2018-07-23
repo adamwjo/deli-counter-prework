35116a41fe718ec86dc1344794b5f9c935b716b6
Adam Johnson <adamwjo@gmail.com>
2018-07-23 20:21:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ end
 def take_a_number(array, name)
   array.push(name)
   x = array.index(name) + 1
-  puts "Welcome, #{name}. You are number #{x} in line "
+  puts "Welcome, #{name}. You are number #{x} in line. "
 end
